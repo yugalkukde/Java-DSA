03be2636e6a4e561f33bc818b9b890a182e9e61f
u=yugalkukde <yugalkukde7@gmail.com>
2024-05-19 11:50:43 +0530

    removed some commented duplicate methods

--- src/main/java/org/yugal/Solution.java
+++ src/main/java/org/yugal/Solution.java
@@ -204,20 +204,6 @@ class Solution {
         return false;
     }
 
-    /*public static int binarySearch(int[] numbersArray, int target) {
-        int left = 0, right = numbersArray.length - 1, mid;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (numbersArray[mid] == target)
-                return mid;
-            else if (numbersArray[mid] < target)
-                left = mid + 1;
-            else //if (numbersArray[mid] < numbersArray[left])
-                right = mid - 1;
-        }
-        return -1;
-    }*/
-
     public static int binarySearch(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;

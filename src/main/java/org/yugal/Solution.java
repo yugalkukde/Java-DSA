9b1aaf223702b264b75185f9b37f43de18f556d5
u=yugalkukde <yugalkukde7@gmail.com>
2024-05-19 11:47:18 +0530

    merged methods from p-pc into this project

--- src/main/java/org/yugal/Solution.java
+++ src/main/java/org/yugal/Solution.java
@@ -1,5 +1,7 @@
 package org.yugal;
 
+import java.util.Arrays;
+
 class Solution {
 
 
@@ -7,9 +9,17 @@ class Solution {
 
         int[] numbers = {2, 3, 5, 6, 8};
         int[] numbers2 = {1, 4, 7, 19};
+        int[] numbersArray = {1, 2, 3, 4, 5, 7, 9, 65, 78, 79, 89, 90, 91, 92, 93, 100};
+        int[] numbersArray1 = {-3, 4, 0, -2, 6, -1};
+        int[] numbersArray2 = {45, 34, 678, 89, 9, 0};
         //System.out.println("ans : " + Arrays.toString(merge(numbers, numbers2)));
         System.out.println("ans : " + stringSearch("please help me i need holp help and also help her", "help"));
         //System.out.println("hello".substring(0, "hello".length() - 1));
+        //System.out.println(binarySearch(numbersArray, 79));
+        //System.out.println(countUniqueValues(numbersArray1));
+        //System.out.println(Arrays.toString(insertionSort(numbersArray2)));
+        //System.out.println(Arrays.toString(insertionSort(Arrays.stream(numbersArray2).boxed().collect(Collectors.toList()))));
+
     }
 
     public static int[] mergeSort(int[] arr) {
@@ -90,6 +100,123 @@ class Solution {
         num2--;
         return num1 * power(num1, num2);
     }
+
+    public static int[] insertionSort(int[] arrInp) {
+        for (int i = 1; i < arrInp.length; i++) {
+            int curr = arrInp[i], j;
+            for (j = i - 1; j >= 0 && arrInp[j] > curr; j--) {
+                arrInp[j + 1] = arrInp[j];
+            }
+            arrInp[j + 1] = curr;
+        }
+
+        return arrInp;
+    }
+
+    public static int[] selectionSort(int[] arrInp) {
+        int currSmallest;
+        for (int i = 0; i < arrInp.length; i++) {
+            currSmallest = i;
+            for (int j = i + 1; j < arrInp.length; j++) {
+                if (arrInp[j] < arrInp[currSmallest]) {
+                    currSmallest = j;
+                }
+            }
+            if (i != currSmallest) {
+                arrInp = swap(arrInp, currSmallest, i);
+            }
+        }
+        return arrInp;
+    }
+
+    public static int[] bubbleSort(int[] arrInp) {
+        boolean noSwap;
+        for (int i = arrInp.length; i > 0; i--) {
+            noSwap = true;
+            for (int j = 0; j < i - 1; j++) {
+                if (arrInp[j] > arrInp[j + 1]) {
+                    arrInp = swap(arrInp, j, j + 1);
+                    noSwap = false;
+                }
+            }
+            if (noSwap) break;
+        }
+        return arrInp;
+    }
+
+    public static int[] swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        return array;
+    }
+
+    public static int maxSubarraySum(int[] arr, int len) {
+        if (arr.length == 0 || arr.length < len) return -1;
+        int maxCount = 0, temp;
+        for (int i = 0; i < len; i++) {
+            maxCount += arr[i];
+        }
+        temp = maxCount;
+        for (int j = len; j < arr.length; j++) {
+            temp = (temp - arr[j - len]) + arr[j];
+            if (temp > maxCount) maxCount = temp;
+        }
+        return maxCount;
+    }
+
+    public static boolean isSubsequence(String st1, String st2) {
+        int st1Pointer = 0, st2Pointer;
+        StringBuilder st1Copy = new StringBuilder();
+        for (st2Pointer = 0; st2Pointer < st2.length(); st2Pointer++) {
+            if (st1Pointer == st1.length()) break;
+            if (st1.charAt(st1Pointer) == st2.charAt(st2Pointer)) {
+                st1Copy.append(st1.charAt(st1Pointer));
+                st1Pointer++;
+            }
+        }
+        return st1Copy.length() == st1.length();
+    }
+
+    public static boolean averagePair(int[] numsArray, double target) {
+
+        if (numsArray.length < 2) return false;
+
+        int left = 0, right;
+        for (right = 1; right < numsArray.length; right++) {
+            System.out.println(numsArray[left] + " : " + numsArray[right]);
+            System.out.println("Average : " + (double) (numsArray[left] + numsArray[right]) / 2);
+            if ((double) (numsArray[left] + numsArray[right]) / 2 == target) return true;
+            else left++;
+        }
+
+        return false;
+    }
+
+    public static boolean areThereDuplicates(String... inp) {
+        if (inp.length == 0) return false;
+        Arrays.sort(inp);
+        int left = 0, right;
+        for (right = 1; right < inp.length; right++) {
+            if (inp[left].equals(inp[right])) left++;
+            else return true;
+        }
+        return false;
+    }
+
+    /*public static int binarySearch(int[] numbersArray, int target) {
+        int left = 0, right = numbersArray.length - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (numbersArray[mid] == target)
+                return mid;
+            else if (numbersArray[mid] < target)
+                left = mid + 1;
+            else //if (numbersArray[mid] < numbersArray[left])
+                right = mid - 1;
+        }
+        return -1;
+    }*/
 
     public static int binarySearch(int[] arr, int target) {
         int left = 0;
